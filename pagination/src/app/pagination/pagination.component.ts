04640f3d659ce13b68223aeff2522009689c48b5
tjansky <tomislav.jansky@gmail.com>
2021-10-28 20:47:01 +0200

    Fixed bug when duplicate squares appear

--- pagination/src/app/pagination/pagination.component.ts
+++ pagination/src/app/pagination/pagination.component.ts
@@ -12,7 +12,7 @@ export class PaginationComponent implements OnInit {
   @Input() currentPage: number = 1;
   @Input() totalPages: number = 11;
   @Input() pageSize: number = 10;
-  @Input() totalCount: number = 108;
+  @Input() totalCount: number = 10;
 
   firstSquareList: number[] = [];
   middleSquareList: number[] = [];
@@ -39,48 +39,60 @@ export class PaginationComponent implements OnInit {
     this.lastSquareList = [];
 
     const isAtStart = squareNumber < this.startEndCount;
-    const isAtEnd = squareNumber > this.totalCount-(this.startEndCount-1);
+    const isAtEnd = squareNumber > this.totalCount - (this.startEndCount - 1);
     const isInMiddle = !isAtStart && !isAtEnd;
 
-    // if current page is less than startCount show first squares
-    if (isAtStart) {
-      this.leftDots = true;
+    const isShort = this.totalCount <= (this.startEndCount + 3);
+
+    // if there is not enough squares only one list is required
+    if (isShort) {
+      this.leftDots = false;
       this.rightDots = false;
+
       let i = 1;
-      while (i < this.startEndCount+1) {
+      while (i < this.totalCount + 1) {
         this.firstSquareList.push(i);
         i++;
-      } 
-    } else {
+      }
+    } else { // if list isnt short
+      // if current page is less than startCount show first squares
+      if (isAtStart) {
+        this.leftDots = true;
+        this.rightDots = false;
+        let i = 1;
+        while (i < this.startEndCount + 1) {
+          this.firstSquareList.push(i);
+          i++;
+        }
+      } else {
         this.firstSquareList = [1];
-    }
+      }
 
-    // if current page is at the end show last squares
-    if(isAtEnd) {
-      this.leftDots = true;
-      this.rightDots = false;
-      let i = this.totalCount-(this.startEndCount-1);
-      while (i <= this.totalCount) {
-        this.lastSquareList.push(i);
-        i++;
-      } 
-    } else {
-      this.lastSquareList = [this.totalCount];
-    }
+      // if current page is at the end show last squares
+      if (isAtEnd) {
+        this.leftDots = true;
+        this.rightDots = false;
+        let i = this.totalCount - (this.startEndCount - 1);
+        while (i <= this.totalCount) {
+          this.lastSquareList.push(i);
+          i++;
+        }
+      } else {
+        this.lastSquareList = [this.totalCount];
+      }
 
-    // if current page is in the middle: show first, last and 3 of middle squares
-    if (isInMiddle) {
-      this.leftDots = true;
-      this.rightDots = true;
-      this.middleSquareList = [squareNumber-1, squareNumber, squareNumber+1];
-    }
+      // if current page is in the middle: show first, last and 3 of middle squares
+      if (isInMiddle) {
+        this.leftDots = true;
+        this.rightDots = true;
+        this.middleSquareList = [squareNumber - 1, squareNumber, squareNumber + 1];
+      }
 
-    // notice parent that new square was clicked
-    if (!isInitial) {
-      this.newSquareClicked.emit({pageNum: squareNumber, pageSize: this.pageSize});
+      // notice parent that new square was clicked
+      if (!isInitial) {
+        this.newSquareClicked.emit({ pageNum: squareNumber, pageSize: this.pageSize });
+      }
     }
-    
-
   }
 
 
